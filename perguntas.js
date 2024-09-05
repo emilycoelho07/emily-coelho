f9e080b9af361058f9cb23102f347071cb9408d9
emilycoelho07 <106595503+emilycoelho07@users.noreply.github.com>
2024-09-05 20:16:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,72 @@
+export const perguntas = [
+    {
+        enunciado: 'Teste seu conhecimento sobre o fascinante Reino de Gelo',
+        alternativas: [
+            {
+                texto: "Qual é o nome da princesa do Reino de Gelo?",
+                afirmacao: [
+                    "Isolde",
+                    "Elara"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        alternativas: [
+            {
+                texto: "O que o vento trazia na noite em que Elara ouviu algo diferente?",
+                afirmacao: [
+                    "Um sussurro misterioso",
+                    "Latido de um cachorro"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Quem é a guardiã do Reino de Gelo?",
+                afirmacao: [
+                    "Isolde",
+                    "Astraia"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        alternativas: [
+            {
+                texto: "O que está acontecendo com o Reino de Gelo que preocupa a guardiã?",
+                afirmacao: [
+                    "O reino está comemorando seus 739 anos",
+                    "O gelo está enfraquecendo e uma escuridão se aproxima"
+                ],
+                proxima: 5,
+            },
+        ]
+    },
+    {
+        alternativas: [
+            {
+                texto: "Onde Elara deve ir para descobrir como salvar o Reino de Gelo?",
+                afirmacao: [
+                    "Na montanha de cristal",
+                    "Na fazenda de sua amiga"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        alternativas: [
+            {
+                texto: "O que Elara deve encontrar na Montanha de Cristal?",
+                afirmacao: [
+                    "Um buquê de flores",
+                    "O espelho da verdade"
+                ],
+            
+            },
+        ]
+    }
+];
+
